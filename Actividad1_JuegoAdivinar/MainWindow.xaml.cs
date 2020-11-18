b7aaa5a1c3b055eb57181b2e50ef4913c5515c02
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-11-18 12:24:33 +0100

    Agregar archivos de proyecto.

---
+++ Actividad1_JuegoAdivinar/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+
+namespace Actividad1_JuegoAdivinar
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int numeroGenerado;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numeroGenerado = GenerarNumAleatorio();
+        }
+        private int GenerarNumAleatorio()
+        {
+            Random seed = new Random();
+            return seed.Next(0, 101);
+        }
+        private void comprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numUser = int.Parse(numeroUserTextBox.Text);
+            if (numUser == numeroGenerado)
+                mensajeTextBlock.Text = "¡CORRECTO!";
+            else if (numUser < numeroGenerado)
+                mensajeTextBlock.Text = "Te has quedado corto";
+            else
+                mensajeTextBlock.Text = "Te has pasado";
+        }
+
+        private void reiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            numeroGenerado = GenerarNumAleatorio();
+            mensajeTextBlock.Text = "";
+            numeroUserTextBox.Text = "";
+        }
+    }
+}
